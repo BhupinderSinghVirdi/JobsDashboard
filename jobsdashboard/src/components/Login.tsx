b11127366a890ece51d7f1fb93c028671b492b17
BhupinderSinghVirdi <n01435365@humbermail.ca>
2023-02-08 22:55:13 -0500

    SkeletonChanges

---
+++ jobsdashboard/src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Login = () => {
+    return(
+        <div>
+            <h1>
+                Hello From Login
+            </h1>
+        </div>
+    )
+}
+
+export default Login;
