13997237897a62ab39771d46c0e00dfff2d7470b
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-02-14 10:34:38 -0500

    feat : Initial Jobs applied changes

---
+++ jobsdashboard/src/components/Jobs/CreateJobsApplied.tsx
@@ -0,0 +1,5 @@
+const CreateJobsApplied = () => {
+
+}
+
+export default CreateJobsApplied;
---
+++ jobsdashboard/src/components/Jobs/ViewJobsApplied.tsx
@@ -0,0 +1,5 @@
+const ViewJobsApplied = () => {
+    return (<p>This is Jobs Applied Page</p>);
+}
+
+export default ViewJobsApplied;
