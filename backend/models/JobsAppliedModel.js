7fe6221108209937435ac30100b1a4d15f7cb1fa
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-02-20 11:32:08 -0500

    refactor : Added job id to jobs applied and fixed the schema

--- backend/models/JobsAppliedModel.js
+++ backend/models/JobsAppliedModel.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-const jobsAppliedModel = new mongoose.Schema({
-    "jobs_applied" : {
-        "type": "Array"
-    }
-})
+var Schema = mongoose.Schema;
 
-module.exports = mongoose.model('JobsAppliedModel', jobsAppliedModel, 'jobsapplied')
+const jobs_appliedSchema = new Schema({
+  date_applied : String,
+  job_id : String
+});
+
+
+JobsAppliedSchema = new Schema({
+  jobs_applied : [jobs_appliedSchema],
+  user_id : String,
+});
+
+module.exports = mongoose.model('JobsApplied', JobsAppliedSchema, 'jobsapplied')
