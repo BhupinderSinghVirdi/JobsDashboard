3e29b274350881fceaad051a3cb86ad2a820c58d
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-03-13 19:57:47 -0400

    refactor : frontend Changes for job details

--- jobsdashboard/src/components/Jobs/JobApplicationForm.tsx
+++ jobsdashboard/src/components/Jobs/JobApplicationForm.tsx
@@ -1,55 +1,88 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudUpload } from '@fortawesome/free-solid-svg-icons';
+import Tickmark from '../../ui/Tickmark';
 
-const JobApplicationForm = () => {
-    return (
-        <div className="mt-8 p-10 bg-white">
-        <h4 className="text-2xl font-medium mb-8">Apply for the job</h4>
-        <form action="#">
-            <div className="flex flex-wrap -mr-4 -ml-4">
-                <div className="md:w-1/2 pr-4 pl-4">
-                    <div>
-                        <input className="h-16 w-full rounded pl-5 mb-5 border border-solid border-neutral-200" type="text" placeholder="Your name"/>
-                    </div>
+type JobApplicationFormType = {
+  user_id:string,
+  job_id:string,
+  handleSubmit: (formData : FormData) => void;
+};
+
+const JobApplicationForm = ({ user_id,job_id,handleSubmit }: JobApplicationFormType) => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [mainFormData,setFormData] = useState<FormData | undefined>()
+    const [showTickMark,setTickMark] = useState<boolean>(false)
+
+    const handleFileSelect = () => {
+      fileInputRef.current?.click();
+    };
+  
+    const handleInputChange = () => {
+      const form = document.getElementById('applicationForm') as HTMLFormElement;
+      const formData = new FormData(form);
+      setFormData(formData);
+    }
+
+    const handleFileChange = () => {
+      const form = document.getElementById('applicationForm') as HTMLFormElement;
+      const formData = new FormData(form);
+      setFormData(formData);
+      const files = fileInputRef.current?.files;
+      if (files && files.length > 0) {
+        const file = files[0];
+        formData.append('file', file);
+        setTickMark(true);
+      }
+    };
+
+  return (
+    <div className="mt-8 p-10 bg-white">
+      <h4 className="text-2xl font-medium mb-8">Apply for the job</h4>
+      <form action="#" id="applicationForm">
+        <div className="flex flex-wrap -mr-4 -ml-4">
+          <div className="md:w-1/2 pr-4 pl-4">
+            <div>
+              <input
+                ref={fileInputRef}
+                type="file"
+                name="file"
+                id="resume"
+                accept=".pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+              <div
+                id="uploadDiv"
+                className="flex flex-col p-4 border border-dashed border-gray-400 rounded-lg text-center cursor-pointer"
+                onClick={handleFileSelect}
+                style={showTickMark ? { pointerEvents: 'none' } : {}}
+              >
+                {!showTickMark && <FontAwesomeIcon id="faUploadIcon" icon={faCloudUpload} className="text-3xl text-gray-400 mb-2" />}
+                {showTickMark && <Tickmark className="self-center"/>}
+                <div id="uploadText" className="text-sm text-gray-600">
+                  {showTickMark ? "File ready to be uploaded" : "Click or drag file to upload" }
                 </div>
-                <div className="md:w-1/2 pr-4 pl-4">
-                    <div>
-                        <input className="h-16 w-full rounded pl-5 mb-5 border border-solid border-neutral-200" type="text" placeholder="Email"/>
-                    </div>
-                </div>
-                <div className="md:w-full pr-4 pl-4">
-                    <div>
-                        <input className="h-16 w-full rounded pl-5 mb-5 border border-solid border-neutral-200" type="text" placeholder="Website/Portfolio link"/>
-                    </div>
-                </div>
-                <div className="md:w-full pr-4 pl-4">
-                    <div className="w-full h-16 rounded mb-5 border border-solid border-neutral-200 flex flex-row">
-                        <div className="-mr-px flex">
-                          <button className="bg-transparent text-base text-gray-600 pl-5 mr-1 border-none" type="button" id="inputGroupFileAddon03">
-                            <FontAwesomeIcon icon={faCloudUpload}/>
-                          </button>
-                        </div>
-                        <div className="mb-0 h-16 border-none items-center relative flex flex-row-reverse flex-auto ">
-                          <input type="file" className="relative h-8 m-0 opacity-0 z-20" id="inputGroupFile03" aria-describedby="inputGroupFileAddon03"/>
-                          <label className="flex-auto z-10 p-0 text-gray-600 bg-transparent rounded-none border-none self-middle" htmlFor="inputGroupFile03">Upload CV</label>
-                        </div>
-                      </div>
-                </div>
-                <div className="md:w-full pr-4 pl-4">
-                    <div>
-                        <textarea  className="h-48 p-5 w-full rounded pl-5 mb-5 border border-solid border-neutral-200" name="#" id="" cols={30} rows={10} placeholder="Coverletter"></textarea>
-                    </div>
-                </div>
-                <div className="md:w-full pr-4 pl-4">
-                    <div className="submit_btn">
-                        <button className="bg-green-500 text-white inline-block pt-3 pr-8 pb-3 pl-8 text-base font-medium rounded text-center capitalize cursor-pointer w-full hover:text-green-500 hover:bg-transparent hover:border hover:border-solid hover:border-green-500" type="submit">Apply Now</button>
-                    </div>
-                </div>
+              </div>
             </div>
-        </form>
+          </div>
+          <div className="md:w-1/2 pr-4 pl-4">
+            <input name="job_id" type="hidden" value={job_id} />
+            <input name="user_id" type="hidden" value={user_id} />
+            <input name="apply_with_name" className="appearance-none w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4" type="text" placeholder="Full Name" required onChange={handleInputChange} />
+            <input name="apply_with_email" className="appearance-none w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4" type="email" placeholder="Email Address" required onChange={handleInputChange}/>
+            <input name="apply_with_phone" className="appearance-none w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4" type="tel" placeholder="Phone Number" required onChange={handleInputChange}/>
+          </div>
+        </div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button" onClick={() => { mainFormData ? handleSubmit(mainFormData) : alert("No form data")}}
+        >
+          Apply Now
+        </button>
+      </form>
     </div>
-    );
+  );
 };
 
 export default JobApplicationForm;
--- jobsdashboard/src/components/Jobs/JobDetailsHeader.tsx
+++ jobsdashboard/src/components/Jobs/JobDetailsHeader.tsx
@@ -3,29 +3,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarker,faClock } from '@fortawesome/free-solid-svg-icons'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
-const JobDetailsHeader = () => {
+type JobDetailsHeaderType = {
+    position:string,
+    location:string,
+    company_logo:string,
+}
+
+const JobDetailsHeader = ({position,location,company_logo} : JobDetailsHeaderType) => {
     return (
     <div className="job_details_header">
         <div className="single_jobs bg-white flex justify-between p-10 border-b-2 border-solid border-b-gray-200">
             <div className="jobs_left flex items-center">
                 <div className="float-left w-20 h-20 p-15 bg-gray-100 mr-10">
-                    {/* <img src="img/svg_icon/1.svg" alt=""/> */}
+                    <img src={company_logo} alt="Logo"/>
                 </div>
                 <div className="jobs_conetent">
-                    <div><h4 className='font-bold text-xl'>Software Engineer</h4></div>
+                    <div><h4 className='font-bold text-xl'>{position}</h4></div>
                     <div className="links_locat flex items-center">
                         <div className="mr-10">
-                            <p className='text-gray-500 mb-0'> <FontAwesomeIcon icon={faMapMarker}/> California, USA</p>
+                            <p className='text-gray-500 mb-0'> <FontAwesomeIcon icon={faMapMarker}/> {location} </p>
                         </div>
                         <div className="mr-10">
-                            <p className='text-gray-500 mb-0'> <FontAwesomeIcon icon={faClock}/> Part-time</p>
+                            {/* <p className='text-gray-500 mb-0'> <FontAwesomeIcon icon={faClock}/> Part-time</p> */}
                         </div>
                     </div>
                 </div>
             </div>
             <div className="jobs_right">
                 <div className="apply_now">
-                    <div className="heart_mark w-10 h-10 leading-10 rounded text-green-400 text-center inline-block bg-green-50 cursor-pointer hover:bg-green-400 hover:text-white "> <FontAwesomeIcon icon={faHeart}/> </div>
+                    {/* <div className="heart_mark w-10 h-10 leading-10 rounded text-green-400 text-center inline-block bg-green-50 cursor-pointer hover:bg-green-400 hover:text-white "> <FontAwesomeIcon icon={faHeart}/> </div> */}
                 </div>
             </div>
         </div>
