7f083957a08466b47f22ea3f06bad1beddf4279d
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-03-13 22:25:58 -0400

    refactor : Added logo and Changed Navigation design

--- jobsdashboard/tailwind.config.js
+++ jobsdashboard/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat-alternates': ['Montserrat Alternates', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
