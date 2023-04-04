ed8ac7992d1fc0f380855a66cea45cfacd9974cc
siddhurc <n01568083@humber.ca>
2023-04-03 22:52:04 -0400

    Admin Login Component using Reactive Forms

---
+++ jobsdashboard-angular/src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+
+
+}
