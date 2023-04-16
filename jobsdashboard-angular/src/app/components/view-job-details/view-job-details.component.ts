56efcc9bf4cbc762fb75c870e83eb07c3d216ea2
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2023-04-16 16:23:10 -0400

    feat : changes for jobs applied and jobs view components

--- jobsdashboard-angular/src/app/components/view-job-details/view-job-details.component.ts
+++ jobsdashboard-angular/src/app/components/view-job-details/view-job-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import axios from 'axios';
 
 @Component({
@@ -9,9 +11,12 @@ import axios from 'axios';
 export class ViewJobDetailsComponent {
   jobDetails: any;
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
   async ngOnInit() {
+    const jobId = this.route.snapshot.params['id'];
     try {
-      const response = await axios.get('http://localhost:3001/api/jobsapplied');
+      const response = await axios.get(`http://localhost:3001/api/jobs/${jobId}`);
       const data = response.data;
       this.jobDetails = data;
     } catch (error) {
--- jobsdashboard-angular/src/app/ui/job-details-view/job-details-view.component.ts
+++ jobsdashboard-angular/src/app/ui/job-details-view/job-details-view.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 type JobsDetailsType = {
   _id: string
@@ -20,19 +18,6 @@ type JobsDetailsType = {
   templateUrl: './job-details-view.component.html',
   styleUrls: ['./job-details-view.component.css']
 })
-export class JobDetailsViewComponent  implements OnInit {
+export class JobDetailsViewComponent {
   @Input() jobDetails!: JobsDetailsType;
-
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
-
-  async ngOnInit() {
-    const jobId = this.route.snapshot.params['id'];
-    try {
-      const response = await this.http.get(`/api/jobs/${jobId}`).toPromise();
-      this.jobDetails = response as any;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
 }
