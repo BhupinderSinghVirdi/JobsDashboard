6c16bf98da6b6e0cb014be86502cc63650101f7b
BhupinderSinghVirdi <n01435365@humbermail.ca>
2023-04-18 01:36:43 -0400

    latestPull

--- jobsdashboard-angular/src/app/app-routing.module.ts
+++ jobsdashboard-angular/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ViewJobDetailsComponent } from './components/view-job-details/view-job-details.component';
 import { ViewJobsAppliedComponent } from './components/view-jobs-applied/view-jobs-applied.component';
 import { LoginComponent } from './components/login/login.component';
+import { AccountComponent } from './components/account/account.component';
 
 const routes: Routes = [ 
 { path: '', redirectTo: 'jobs', pathMatch: 'full' },
 { path: 'jobs-applied', component: ViewJobsAppliedComponent },
 { path: 'jobs-details/:id', component: ViewJobDetailsComponent },
-{ path: 'login', component: LoginComponent}
+{ path: 'login', component: LoginComponent},
+{ path: 'accountDetails', component: AccountComponent }
 ];
 
 @NgModule({
